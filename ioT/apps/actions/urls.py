96868d4f8a34c5de4433123f803b7d075da21c08
Alex <akeymy4@gmail.com>
2018-08-02 01:14:38 -0400

    changing all structure of the project, for make with Raspberri pi and firebase

--- ioT/apps/actions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-
-urlpatterns = patterns('',
-    url(r'^$', 'apps.actions.views.index'),
-    url(r'^ajaxR$', 'apps.actions.views.ajaxR'),
-    url(r'^ajaxData$', 'apps.actions.views.ajaxData'),
-)
--- ioT/ioT/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-
-urlpatterns = patterns('',
-
-    url(r'^',include('apps.actions.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
