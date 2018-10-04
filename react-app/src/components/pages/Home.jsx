f0bd2031d6ea38542ffff85375e7b5af13064490
Alex <akeymy4@gmail.com>
2018-10-03 20:20:53 -0400

    modificando react-js

--- react-app/src/components/pages/Home.jsx
+++ react-app/src/components/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React, {Component} from 'react'
-import Location from './Location'
+import { Button } from 'react-bootstrap'
+
 export default class Home extends Component {
   constructor (...props) {
     super (...props)
   }
   render () {
     return (
-      <Location/>
+      <div>
+        {/* <PageHeader> */}
+          {/* DOMOTICA <small>para vivienda</small> */}
+        {/* </PageHeader> */}
+        <Button bsStyle="success">fsfsd</Button>
+      </div>
     )
   }
 }
